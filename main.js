5460f2a9227e1b9c7f4fb2e285c701362a0f08bd
Simon Andrews <simon.andrews212@gmail.com>
2020-03-21 16:39:08 -0400

    Add 3/21, merge Dukes and Nantucket counties

--- main.js
+++ main.js
@@ -2,13 +2,12 @@ const allDatasets = [
   barnstableDataset,
   berkshireDataset,
   bristolDataset,
-  dukesDataset,
+  dukesAndNantucketDataset,
   essexDataset,
   franklinDataset,
   hampdenDataset,
   hampshireDataset,
   middlesexDataset,
-  nantucketDataset,
   norfolkDataset,
   plymouthDataset,
   suffolkDataset,
@@ -39,7 +38,7 @@ let ctx = document.getElementById('chart').getContext('2d');
 let chart = new Chart(ctx, {
   type: 'bar',
   data: {
-    labels: ['3/9', '3/10', '3/11', '3/12', '3/13', '3/14', '3/15', '3/16', '3/17', '3/18', '3/19', '3/20'],
+    labels: ['3/9', '3/10', '3/11', '3/12', '3/13', '3/14', '3/15', '3/16', '3/17', '3/18', '3/19', '3/20', '3/21'],
     datasets: allDatasets
   },
   options: {
