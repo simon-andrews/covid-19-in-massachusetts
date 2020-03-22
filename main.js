17c3adacad0f9c4712a4fade84baa3572dba4da1
Simon Andrews <simon.andrews212@gmail.com>
2020-03-22 17:41:25 -0400

    3/22 update

--- main.js
+++ main.js
@@ -38,7 +38,7 @@ let ctx = document.getElementById('chart').getContext('2d');
 let chart = new Chart(ctx, {
   type: 'bar',
   data: {
-    labels: ['3/9', '3/10', '3/11', '3/12', '3/13', '3/14', '3/15', '3/16', '3/17', '3/18', '3/19', '3/20', '3/21'],
+    labels: ['3/9', '3/10', '3/11', '3/12', '3/13', '3/14', '3/15', '3/16', '3/17', '3/18', '3/19', '3/20', '3/21', '3/22'],
     datasets: allDatasets
   },
   options: {
